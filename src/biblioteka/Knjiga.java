8e0433ff5fd5049b619a38df9b24b2d4a40b9b5f
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-04-06 13:54:08 +0200

    Dokumentovana klasa Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -3,19 +3,58 @@ package biblioteka;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Predstavlja knjigu u biblioteci.
+ * 
+ * Svaka knjiga ima naslov, ISBN broj, listu autora,
+ * izdavaca i broj izdanja.
+ * 
+ * @author Dusan Petrovic
+ * @version 1.0
+ */
 public class Knjiga {
 
+	/**
+	 * Naslov knjige kao String.
+	 */
 	private String naslov;
+	
+	/**
+	 * ISBN broj knjige.
+	 */
 	private long isbn;
+	
+	/**
+	 * Lista autora knjige.
+	 */
 	private List<Autor> autori;
+	
+	/**
+	 * Naziv izdavaca knjige.
+	 */
 	private String izdavac;
+	
+	/**
+	 * Broj izdanja knjige.
+	 */
 	private int izdanje;
 	
-	
+	/**
+	 * Vraca naslov knjige.
+	 * 
+	 * @return naslov knjige kao String.
+	 */
 	public String getNaslov() {
 		return naslov;
 	}
 	
+	/**
+	 * Postavlja naslov knjige na unetu vrednost.
+	 * 
+	 * @param naslov Novi naslov knjige.
+	 * @throws java.lang.NullPointerException Ako je naslov null.
+	 * @throws java.lang.IllegalArgumentException Ako je naslov prazan String.
+	 */
 	public void setNaslov(String naslov) {
 		if (naslov == null)
 			throw new NullPointerException("Naslov ne sme biti null");
@@ -26,10 +65,21 @@ public class Knjiga {
 		this.naslov = naslov;
 	}
 	
+	/**
+	 * Vraca ISBN broj knjige.
+	 * 
+	 * @return ISBN kao long vrednost.
+	 */
 	public long getIsbn() {
 		return isbn;
 	}
 	
+	/**
+	 * Postavlja ISBN broj knjige.
+	 * 
+	 * @param isbn Novi ISBN broj.
+	 * @throws java.lang.IllegalArgumentException Ako je ISBN manji ili jednak nuli.
+	 */
 	public void setIsbn(long isbn) {
 		if(isbn <= 0)
 			throw new IllegalArgumentException("ISBN mora biti veci od nule");
@@ -37,19 +87,40 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 	
-	
+	/**
+	 * Vraca listu autora knjige.
+	 * 
+	 * @return lista autora.
+	 */
 	public List<Autor> getAutori() {
 		return autori;
 	}
 
+	/**
+	 * Postavlja listu autora knjige.
+	 * 
+	 * @param autori Lista autora.
+	 */
 	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
 
+	/**
+	 * Vraca izdavaca knjige.
+	 * 
+	 * @return izdavac kao String.
+	 */
 	public String getIzdavac() {
 		return izdavac;
 	}
 	
+	/**
+	 * Postavlja izdavaca knjige.
+	 * 
+	 * @param izdavac Novi izdavac.
+	 * @throws java.lang.NullPointerException Ako je izdavac null.
+	 * @throws java.lang.IllegalArgumentException Ako je izdavac prazan String.
+	 */
 	public void setIzdavac(String izdavac) {
 		if (izdavac== null)
 			throw new NullPointerException("Izdavac ne sme biti null");
@@ -60,10 +131,21 @@ public class Knjiga {
 		this.izdavac = izdavac;
 	}
 	
+	/**
+	 * Vraca broj izdanja knjige.
+	 * 
+	 * @return broj izdanja kao int.
+	 */
 	public int getIzdanje() {
 		return izdanje;
 	}
 	
+	/**
+	 * Postavlja broj izdanja knjige.
+	 * 
+	 * @param izdanje Novi broj izdanja.
+	 * @throws java.lang.IllegalArgumentException Ako je izdanje manje ili jednako nuli.
+	 */
 	public void setIzdanje(int izdanje) {
 		if(izdanje <= 0)
 			throw new IllegalArgumentException("Izdanje mora biti vece od nule");
@@ -71,6 +153,12 @@ public class Knjiga {
 		this.izdanje = izdanje;
 	}
 
+	/**
+	 * Vraca String reprezentaciju knjige.
+	 * 
+	 * @return svi podaci o knjizi u formatu:
+	 * "Knjiga [naslov=..., isbn=..., autori=..., izdavac=..., izdanje=...]"
+	 */
 	@Override
 	public String toString() {
 		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
@@ -82,6 +170,19 @@ public class Knjiga {
 		return Objects.hash(isbn);
 	}
 
+	/**
+	 * Poredi dve knjige po ISBN broju.
+	 * 
+	 * @param obj Druga knjiga sa kojom se poredi.
+	 * @return
+	 * <ul>
+	 * 
+	 * <li><b>true</b> ako su obe knjige iste ili imaju isti ISBN.</li>
+	 * 
+	 * <li><b>false</b> ako je objekat null, druge klase ili imaju razlicit ISBN.</li>
+	 * 
+	 * </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -93,8 +194,4 @@ public class Knjiga {
 		Knjiga other = (Knjiga) obj;
 		return isbn == other.isbn;
 	}
-	
-	
-	
-	
 }
